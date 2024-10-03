3aa89cbcc6b4c75da943cb5aef21292c8e99ca09
DeveloperJaat <97823189+DeveloperJaat@users.noreply.github.com>
2024-10-03 19:12:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7153851758:AAFoeI6crXFyHfw-tyDoIe
 API_ID = int(os.environ.get("API_ID", "29593343"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "16dace92844e94a674f5903d94241a56")
+API_HASH = os.environ.get("API_HASH", "9e93c3305814f31695145026944fe9e1")
 
 #Your db channel Id
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002150140332"))
